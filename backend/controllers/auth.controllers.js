1f2d492539416c3c66c124bd6ed62dfa3de7ae03
Gayatri Sanap <159698245+2004Gayatri@users.noreply.github.com>
2026-02-13 22:33:04 +0530

    Modify cookie settings for signUp and Login
    Updated cookie settings for secure transmission.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -29,8 +29,8 @@ export const signUp = async (req , res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge : 7*24*60*60*1000,
-            sameSite : "strict",
-            secure : false
+            sameSite : "none",
+            secure : true
         });
 
         return res.status(201).json(user);
@@ -64,8 +64,8 @@ export const Login = async (req , res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge : 7*24*60*60*1000,
-            sameSite : "strict",
-            secure : false
+            sameSite : "none",
+            secure : true
         });
 
         return res.status(200).json(user);
@@ -86,4 +86,5 @@ export const logOut = async (req,res)=>{
     }catch(err){
          return res.status(500).json({message : `logout error ${err}`});
     }
+
 }
